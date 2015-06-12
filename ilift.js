7e8d8cf494474f18c84520c57764b794671dbb0a
Omar Erminy <omrsin@gmail.com>
2015-06-13 00:15:49 +0200

    Invokes the service for the sessions with the real values and correct attribute names

--- ilift.js
+++ ilift.js
@@ -5,13 +5,17 @@ function appendTable(session) {
 		<tr> \
 			<td>"+session.date+"</td> \
 			<td>"+session.exercise.name+"</td> \
-			<td>"+session.equipment.item+" "+session.equipment.weight+"</td> \
-			<td>"+3+"</td> \
+			<td>"+session.equipment.type.name+" - "+session.equipment.weightKg+"Kg</td> \
+			<td>"+session.repetitions+"</td> \
 		</tr> \
 	");
 }
 
-$(function(){	
+$(function() {
+
+	/* This invokes a web service accorging to the indicated username.
+	   It handles some HTML manipulation to show the data correctly.
+	 */
 	$("#getSessions").on("click", function(e) {
 		e.preventDefault();
 		var userId = $("#userId").val();
@@ -21,7 +25,7 @@ $(function(){
 			console.log("Empty");
 		} else {
 			$(".alert-submit").hide();
-			$("#userId").parent().removeClass("has-error");
+			$("#userId").parent().removeClass("has-error");			
 			$.getJSON(GET_SESSIONS_BY_USER_ID+userId, function(sessions) {
 				if(sessions.length == 0){
 					$("#sessionTableContainer").hide();
