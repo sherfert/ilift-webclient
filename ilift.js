9fad3152c9f64158829b34d0651a2ebb8ba87c0a
Omar Erminy <omrsin@gmail.com>
2015-06-12 23:01:57 +0200

    Connects to the service provided to obtain the sessions and has come control on the input params

--- ilift.js
+++ ilift.js
@@ -1,23 +1,33 @@
-function appendTable(album) {
+const GET_SESSIONS_BY_USER_ID = "http://130.83.110.244:8080/ilift/session/byUserId/";
+
+function appendTable(session) {
 	$("#sessionsTable > tbody:last-child").append(" \
 		<tr> \
-			<td>12/06/2015</td> \
-			<td>"+album.userId+"</td> \
-			<td>"+album.id+"</td> \
-			<td>"+album.title+"</td> \
+			<td>"+session.date+"</td> \
+			<td>"+session.exercise.name+"</td> \
+			<td>"+session.equipment.item+" "+session.equipment.weight+"</td> \
+			<td>"+3+"</td> \
 		</tr> \
 	");
 }
 
 $(function(){	
 	$("#getSessions").on("click", function(e) {
-		e.preventDefault();		
-		$.getJSON( "http://jsonplaceholder.typicode.com/albums", function(albums) {
-			$("#sessionsTable > tbody").empty();
-			$.each(albums, function(index, value){				
-				appendTable(value);
-				console.log(value.userId);
-			});
-		});		
+		e.preventDefault();
+		var userId = $("#userId").val();
+		if(!userId){
+			$(".alert-submit").show();
+			$("#userId").parent().addClass("has-error");
+			console.log("Empty");
+		} else {
+			$(".alert-submit").hide();
+			$("#userId").parent().removeClass("has-error");
+			$.getJSON(GET_SESSIONS_BY_USER_ID+userId, function(sessions) {
+				$("#sessionsTable > tbody").empty();
+				$.each(sessions, function(index, value){				
+					appendTable(value);				
+				});
+			});		
+		}
 	});
 })
