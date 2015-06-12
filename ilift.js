2495af7bb720e71ba276bfbc28352034a48bc92e
Omar Erminy <omrsin@gmail.com>
2015-06-12 21:11:42 +0200

    Connects to a mock webservice to fill the sessions table

--- ilift.js
+++ ilift.js
@@ -1,3 +1,23 @@
+function appendTable(album) {
+	$("#sessionsTable > tbody:last-child").append(" \
+		<tr> \
+			<td>12/06/2015</td> \
+			<td>"+album.userId+"</td> \
+			<td>"+album.id+"</td> \
+			<td>"+album.title+"</td> \
+		</tr> \
+	");
+}
+
 $(function(){	
-	
+	$("#getSessions").on("click", function(e) {
+		e.preventDefault();		
+		$.getJSON( "http://jsonplaceholder.typicode.com/albums", function(albums) {
+			$("#sessionsTable > tbody").empty();
+			$.each(albums, function(index, value){				
+				appendTable(value);
+				console.log(value.userId);
+			});
+		});		
+	});
 })
